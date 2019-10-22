91f666cc03caf93d21740d0cf88f39ce28ec9e74
unknown <arian.angoma@tecsup.edu.pe>
2019-10-21 20:44:52 -0500

    first commit

---
+++ University/Migrations/201910220131128_v3.cs
@@ -0,0 +1,18 @@
+﻿namespace University.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v3 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.OfficeAssignments", "Activo", c => c.Boolean());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.OfficeAssignments", "Activo");
+        }
+    }
+}
